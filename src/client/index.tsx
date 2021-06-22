542dfce38199e6267ebe4bfd60948b035537d81c
Graham Bull <graham.bull@gmail.com>
2021-06-22 11:54:05 +0100

    feat: implement redux persist
    closes #29

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from './App';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -1,4 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { episode } from './episode';
 import { pushSubscription } from './push_subscription';
@@ -8,17 +10,28 @@ import { show } from './show';
 import { subscription } from './subscription';
 import { user } from './user';
 
+const reducers = combineReducers({
+  user: user.reducer,
+  search: search.reducer,
+  subscription: subscription.reducer,
+  show: show.reducer,
+  episode: episode.reducer,
+  schedule: schedule.reducer,
+  pushSubscription: pushSubscription.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    user: user.reducer,
-    search: search.reducer,
-    subscription: subscription.reducer,
-    show: show.reducer,
-    episode: episode.reducer,
-    schedule: schedule.reducer,
-    pushSubscription: pushSubscription.reducer,
-  },
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
