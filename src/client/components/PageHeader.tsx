814d6ff889608be07f2545c00d2477874553e78a
Graham Bull <graham.bull@gmail.com>
2021-06-23 12:06:39 +0100

    feat: added a skeleton to pageheader
    #16

--- src/client/components/PageHeader.tsx
+++ src/client/components/PageHeader.tsx
@@ -7,6 +7,7 @@ import { BackButton } from './BackButton';
 interface Props {
   className?: string;
   goBack?: boolean;
+  isLoading?: boolean;
   title: string;
   rightElement?: ReactNode;
 }
@@ -14,41 +15,45 @@ interface Props {
 export function PageHeader({
   className,
   goBack,
+  isLoading,
   title,
   rightElement,
 }: Props): ReactElement {
-  const classes = {
-    header: cn('w-full', 'h-12', 'flex', 'items-center', 'p-2', className),
-    title: cn(
-      'mx-2',
-      'font-medium',
-      'text-lg',
-      'overflow-x-scroll',
-      'whitespace-nowrap'
-    ),
-  };
-
-  if (!goBack) {
-    return (
-      <>
-        <Helmet>
-          <title>{title} - Lini</title>
-        </Helmet>
-        <header className={cn(classes.header, 'justify-center')}>
-          <h1 className={cn(classes.title)}>{title}</h1>
-        </header>
-      </>
-    );
-  }
   return (
     <>
       <Helmet>
         <title>{title} - Lini</title>
       </Helmet>
-      <header className={cn(classes.header, 'justify-between')}>
-        <BackButton />
-        <h1 className={cn(classes.title)}>{title}</h1>
-        <div className={cn('w-11')}>{rightElement}</div>
+      <header
+        className={cn(
+          'w-full',
+          'h-12',
+          'flex',
+          'items-center',
+          'p-2',
+          { 'justify-between': goBack, 'justify-center': !goBack },
+          className
+        )}
+      >
+        {goBack && <BackButton />}
+        {isLoading ? (
+          <div
+            className={cn('w-2/3', 'h-3/4', 'animate-pulse', 'bg-gray-600')}
+          />
+        ) : (
+          <h1
+            className={cn(
+              'mx-2',
+              'font-medium',
+              'text-lg',
+              'overflow-x-scroll',
+              'whitespace-nowrap'
+            )}
+          >
+            {title}
+          </h1>
+        )}
+        {goBack && <div className={cn('w-11')}>{rightElement}</div>}
       </header>
     </>
   );
@@ -57,5 +62,6 @@ export function PageHeader({
 PageHeader.defaultProps = {
   className: undefined,
   goBack: true,
+  isLoading: false,
   rightElement: undefined,
 };
