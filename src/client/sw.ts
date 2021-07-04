85c327756a1e9d6108bae24d90a20e3545415977
Graham Bull <graham.bull@gmail.com>
2021-07-04 11:47:15 +0100

    fix: maskable badge for notifications incorrect
    Another attempt to fix this.
    
    #24

--- src/client/sw.ts
+++ src/client/sw.ts
@@ -29,7 +29,7 @@ sw.addEventListener('push', (event) => {
     sw.registration.showNotification(notificationDto.title, {
       body: `${date}\n\n${notificationDto.message}`,
       icon: notificationDto.icon,
-      badge: process.env.CLIENT_URL + '/android-chrome-192x192.png',
+      badge: process.env.CLIENT_URL + '/maskable-icon.png',
       data: { url: notificationDto.url },
     });
   }
