0a6887420bc6b459e1337e155f15ce33537204c8
Graham Bull <graham.bull@gmail.com>
2021-07-08 15:08:29 +0100

    test: show view snapshot

---
+++ src/client/views/Show.test.tsx
@@ -0,0 +1,109 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+
+import { createLocation, createMemoryHistory } from 'history';
+import React from 'react';
+import { match } from 'react-router-dom';
+
+import { ShowWithEpisodesDto } from '../../server/show/show.dto';
+import { RootState } from '../redux/store';
+import { testSetup } from '../utils/testSetup';
+import { Show } from './Show';
+
+const showWithEpisodeDto: ShowWithEpisodesDto = {
+  id: 216,
+  name: 'Rick and Morty',
+  type: 'Animation',
+  language: 'English',
+  genres: ['Comedy', 'Adventure', 'Science-Fiction'],
+  status: 'Running',
+  runtime: 30,
+  runtimeAverage: 30,
+  datePremiered: '2013-12-02',
+  officialSite: 'http://www.adultswim.com/videos/rick-and-morty',
+  scheduleTime: '23:00:00',
+  scheduleDays: ['Sunday'],
+  rating: 9,
+  imageMedium: '/uploads/images/medium_portrait/1/3603.jpg',
+  imageOriginal: '/uploads/images/original_untouched/1/3603.jpg',
+  summary:
+    'Rick is a mentally gifted, but sociopathic and alcoholic scientist and a grandfather to Morty; an awkward, impressionable, and somewhat spineless teenage boy. Rick moves into the family home of Morty, where he immediately becomes a bad influence.',
+  network: {
+    id: 10,
+    name: 'Adult Swim',
+    country: {
+      name: 'United States',
+      code: 'US',
+      timezone: 'America/New_York',
+    },
+  },
+  webChannel: null,
+  episodes: [
+    {
+      id: '14308',
+      name: 'Pilot',
+      season: 1,
+      number: 1,
+      type: 'regular',
+      airdate: '2013-12-02',
+      airtime: '22:30:00',
+      airstamp: '2013-12-03T03:30:00.000Z',
+      runtime: 30,
+      imageMedium: '/uploads/images/medium_landscape/292/730352.jpg',
+      imageOriginal: '/uploads/images/original_untouched/292/730352.jpg',
+      summary:
+        "Rick takes Morty to another dimension to get some seeds for him but Morty's parents are considering to put Rick in a retirement home for keeping Morty away from school to help him in his lab.",
+    },
+  ],
+};
+
+describe('Show View', () => {
+  const history = createMemoryHistory();
+  const path = '/route/:id';
+  const match: match<{ id: string }> = {
+    isExact: false,
+    path,
+    url: path.replace(':id', '1'),
+    params: { id: '1' },
+  };
+  const location = createLocation(match.url);
+
+  it('matches the snapshot when loading', () => {
+    const { asFragment, dispatch } = testSetup(
+      <Show history={history} location={location} match={match} />,
+      { history }
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(dispatch).toBeCalled();
+  });
+
+  it('matches the snapshot when there is an error', () => {
+    const initialState: Partial<RootState> = {
+      show: { status: 'error', error: 'Generic error' },
+    };
+
+    const { asFragment } = testSetup(
+      <Show history={history} location={location} match={match} />,
+      { history, state: initialState }
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('matches the snapshot when there is an episode', () => {
+    const initialState: Partial<RootState> = {
+      show: { status: 'idle', data: showWithEpisodeDto },
+    };
+
+    const { asFragment } = testSetup(
+      <Show history={history} location={location} match={match} />,
+      { history, state: initialState }
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
